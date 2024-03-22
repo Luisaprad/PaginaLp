3d96b99ab4c4a113a8a9d3a4ff78eeb7a37cf1de
Fer <159787283+Luisaprad@users.noreply.github.com>
2024-03-22 11:27:52 -0500

    Add files via upload

---
+++ Mocap-20240321T214206Z-001/Mocap/app.js
@@ -0,0 +1,8 @@
+function abririmagen(){
+    var pope = document.getElementsByClassName("")[0];	
+    if(pope.style.visibility == "proyecto"){
+        pope.style.visibility == "visible";
+    }else{
+        pope.style.visibility == "proyecto";
+    }
+}
